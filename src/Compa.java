4a47935295a89b0d28e2b25c8ae05279529ce5fa
Dewstend <j2a5q7b4@gmail.com>
2018-10-26 15:50:42 -0400

    Removed unnecessary Array import.

--- src/Compa.java
+++ src/Compa.java
@@ -1,6 +1,3 @@
-
-import java.util.Arrays;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -20,14 +17,14 @@ public class Compa {
     public static void main(String[] args) {
         Conn BDD = new Conn();
         BDD.generateSymbols();
-        printDatabase(BDD);
+        //printDatabase(BDD);
         CompoJFrame frame = new CompoJFrame();
         frame.setDefaultCloseOperation(CompoJFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
     
-    public static void printDatabase(Conn Database) {
+    public static void printSymbols(Conn Database) {
         
         for (String Simbolos: Database.symbolsEspeciales) {
             System.out.println(Simbolos);
